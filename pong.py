be91d13253282196102f8ac500f7adcddf54b846
EndlessThoughts <55556079+EndlessThoughts@users.noreply.github.com>
2020-12-24 16:34:04 -0500

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,14 +1,20 @@
 import pygame, sys, random
 
 def ballMovement():
-    global bspeedx, bspeedy
+    global bspeedx, bspeedy, opponentScore, playerScore, getReady
     ball.x += bspeedx
     ball.y += bspeedy
     
     if ball.top <= 0 or ball.bottom >= screen_height:
         bspeedy *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
+    if ball.left <= 0:
+        playerScore += 1
         ballrestart()
+        getReady = pygame.time.get_ticks()
+    if ball.right >= screen_width:
+        opponentScore += 1
+        ballrestart()
+        getReady = pygame.time.get_ticks()
     if ball.colliderect(player) or ball.colliderect(opposition):
         bspeedx *= -1
 
@@ -31,18 +37,25 @@ def oppositionMovement():
         opposition.bottom = screen_height
 
 def ballrestart():
-    global bspeedx, bspeedy
+    global bspeedx, bspeedy, getReady
+    
+    current_time = pygame.time.get_ticks()
     ball.center = (screen_width/2, screen_height/2)
-    bspeedy *= random.choice((1,-1))
-    bspeedx *= random.choice((1,-1))
+    
+    if current_time - getReady < 2100:
+        bspeedx, bspeedy = 0,0
+    else:
+        bspeedy = 7 * random.choice((1,-1))
+        bspeedx = 7 * random.choice((1,-1))
+        getReady = False
 
 #General Setup
 pygame.init()
 clock = pygame.time.Clock()
 
 #main window
-screen_width = 1280
-screen_height = 960
+screen_width = 1000
+screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
 
@@ -58,6 +71,15 @@ bspeedx = 7 * random.choice((1,-1))
 bspeedy = 7 * random.choice((1,-1))
 playerspeed = 0
 oppositionspeed = 7
+
+playerName = "Player"
+oppositionName = "Computer"
+playerScore = 0
+opponentScore = 0
+gameFont = pygame.font.Font("freesansbold.ttf", 32)
+
+getReady = True
+
 
 while True:
     #For input
@@ -87,6 +109,18 @@ while True:
     pygame.draw.ellipse(screen,light_blue,ball)
     pygame.draw.aaline(screen,light_blue,(screen_width/2,0), (screen_width/2,screen_height))
     
+    if getReady:
+        ballrestart()
+        
+    playerUsername = gameFont.render(f"{playerName}",False,light_blue)
+    screen.blit(playerUsername,(530,200))
+    playScore = gameFont.render(f"{playerScore}",False,light_blue)
+    screen.blit(playScore,(525,350))
+    
+    oppsName = gameFont.render(f"{oppositionName}",False,light_blue)
+    screen.blit(oppsName,(250,200))
+    oppScore = gameFont.render(f"{opponentScore}",False,light_blue)
+    screen.blit(oppScore,(455,350))
     
     #Updating the window
     pygame.display.flip()
